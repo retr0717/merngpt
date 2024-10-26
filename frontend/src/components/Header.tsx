175708a24a981e053c1ab8a9665d6344afa0458f
retr0717 <alwinaji717@gmail.com>
2024-10-26 10:39:13 +0530

    nav links and header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,11 +1,55 @@
 import { AppBar, Toolbar } from '@mui/material';
 import Logo from './shared/Logo';
+import { useAuth } from '../context/AuthContext';
+import NavigationLink from './shared/NavigationLink';
 
 const Header = () => {
+    const auth = useAuth();
   return (
    <AppBar sx={{bgcolor: "transparent", position: "static", boxShadow: "none"}}>
     <Toolbar sx={{display: "flex"}}>
         <Logo/>
+        <div>
+            {
+                auth?.isLoggedIn ?
+                (
+                    <>
+                         <NavigationLink
+                            bg="#57cac1"
+                            to="/chat"
+                            text="Go To Chat"
+                            textColor="black"
+                        />
+                        <NavigationLink
+                            bg="#320eb0"
+                            textColor="white"
+                            to="/"
+                            text="logout"
+                            onClick={auth.logout}
+                        />
+                    </>
+                )
+                :
+                (
+                    <>
+                        <NavigationLink
+                            bg="#57cac1"
+                            to="/login"
+                            text="Login"
+                            textColor="black"
+                            onClick={auth?.login}
+                        />
+                        <NavigationLink
+                            bg="#320eb0"
+                            textColor="white"
+                            to="/signup"
+                            text="Signup"
+                            onClick={auth?.signup}
+                        />
+                    </>
+                )
+            }
+        </div>
     </Toolbar>
    </AppBar>
   )
