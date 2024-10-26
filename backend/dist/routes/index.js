7d0625f64475e38e9ea1a979faee85bd1666599d
retr0717 <alwinaji717@gmail.com>
2024-10-26 07:46:38 +0530

    ui intial setup

--- backend/dist/routes/index.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import userRouter from "./user-routes.js";
-import chatRouter from "./chat-routes.js";
-const router = Router();
-router.use("/user", userRouter);
-router.use("/chat", chatRouter);
-export default router;
-//# sourceMappingURL=index.js.map
--- backend/dist/routes/user-routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getAllUsers, login, userSignup } from "../controller/user-controller.js";
-import { loginValidator, signUpValidator, validate } from "../utils/validators.js";
-const userRouter = Router();
-userRouter.get("/", getAllUsers);
-userRouter.post("/signup", validate(signUpValidator), userSignup);
-userRouter.post("/login", validate(loginValidator), login);
-export default userRouter;
-//# sourceMappingURL=user-routes.js.map
