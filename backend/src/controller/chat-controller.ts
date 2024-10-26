d06b3ac9a0f2ff347031448db422e872ebd50452
retr0717 <alwinaji717@gmail.com>
2024-10-26 16:34:15 +0530

    chat completion backend

---
+++ backend/src/controller/chat-controller.ts
@@ -0,0 +1,37 @@
+import { NextFunction, Request, Response } from "express";
+import User from "../models/User.js";
+import { configureOpenAI } from "../config/openai-config.ts";
+import OpenAI from "openai";
+import { ChatCompletionMessageParam } from "openai/resources/index.mjs";
+
+export const generateChatCompletion = async (req : Request, res: Response, next: NextFunction) => {
+    try {
+        const {message} = req.body;
+        const user = await User.findById(res.locals.jwtData.id);
+        if(!user) return res.status(401).json({message: "user not found or token malfunction!"});
+
+        //grab all chats of user.
+        const chats = user.chats.map(({role,content}) => ({role, content})) as ChatCompletionMessageParam[];
+        chats.push({content: message, role: "user"});
+        user.chats.push({content: message, role: "user"});
+
+        //sent all chats with new one to openAI API.
+        const config = configureOpenAI();
+        const openai = new OpenAI(config);
+
+        //get latest response.
+        const chatCompletion = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: chats,
+        }); 
+        
+        user.chats.push(chatCompletion.choices[0].message);
+        await user.save();
+        return res.status(200).json({chats: user.chats});
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message : "something went wrong!"})
+    }
+    
+}
--- backend/src/routes/chat-routes.ts
+++ backend/src/routes/chat-routes.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { verifyToken } from "../utils/token-manager.js";
+import { chatCompletionValidator, validate } from "../utils/validators.js";
+import { generateChatCompletion } from "../controller/chat-controller.js";
 
 const chatRouter = Router();
+chatRouter.post("/new", validate(chatCompletionValidator), verifyToken , generateChatCompletion);
 
 export default chatRouter;
