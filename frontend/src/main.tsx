6fbfab91df4cc46c519cc3d0b3c6e8dfd09c9193
retr0717 <alwinaji717@gmail.com>
2024-10-26 09:28:31 +0530

    Logo, header

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,9 +6,9 @@ import App from './App.tsx'
 import { createTheme, ThemeProvider } from '@mui/material'
 
 const theme = createTheme({
-  typography :{
-    fontFamily: "Roboto Slab, serif",
-    allVariants: { color: "white"},
+  typography: {
+    fontFamily: "Roboto Slab,serif",
+    allVariants: { color: "white" },
   },
 });
 
